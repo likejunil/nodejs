9b5d9deca3c29069f01a90016ad4121a7ecc47b5
권준일 <likejunil@gmail.com>
2022-12-23 16:40:41 +0900

    FEAT: http 모듈 사용법을 학습하라

--- src/lecture/ellie/10_httpOk.js
+++ src/lecture/ellie/10_httpOk.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
 /*
 console.log(http.METHODS);
@@ -13,19 +14,23 @@ const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
     console.log(req.headers);
+    console.log(req.body);
     
     // 확인
-    const welcomeHtml = './html/welcome.html';
-    const notFoundHtml = './html/not-found.html';
+    const htmlDir = 'data/html'
+    const NOT_FOUND = 'not-found.html';
+    const WELCOME = 'welcome.html';
+    const welcomeHtml = path.join(__dirname, htmlDir, WELCOME);
+    const notFoundHtml = path.join(__dirname, htmlDir, NOT_FOUND);
     
     // 요청 정보에 따른 분기
     const url = req.url;
     res.setHeader('Content-Type', 'text/html');
-    if (url === '/it') {
+    if (url === '/welcome') {
         fs.createReadStream(welcomeHtml).pipe(res);
     } else {
         fs.createReadStream(notFoundHtml).pipe(res);
     }
 });
 
-server.listen(8080);
+server.listen(8088);
