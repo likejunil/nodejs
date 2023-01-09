4d4074335bb24fc9e4029af987d098054d4e3452
권준일 <likejunil@gmail.com>
2023-01-09 10:12:24 +0900

    FEAT: 아직 멀었다. ㅜㅠ

--- src/lecture/functional/lesson_1/chapter_1/04_find_findindex_some_every.js
+++ src/lecture/functional/lesson_1/chapter_1/04_find_findindex_some_every.js
@@ -1,9 +1,92 @@
-
+const data = require('./data.js');
+const {go, keys, map, filter, get, identity, negate} = require('./fp.js');
 
 /**
  * _values
+ * _pluck
+ * _reject
+ * _compact
  * _find
  * _find_index
  * _some
  * _every
  */
+
+/*
+const _values = (data) => {
+    // return map(data, identity);
+    return map(identity)(data);
+};
+ */
+const _values = map(identity);
+console.log(data[1]);
+console.log(_values(data[1]));
+console.log(data[2]);
+console.log(_values(data[2]));
+
+
+const _pluck = (data, key) => map(data, get(key))
+console.log(_pluck(data, 'name'));
+console.log(_pluck(data, 'age'));
+
+
+const _reject = (data, cond) => {
+    return filter(data, negate(cond));
+};
+console.log(_reject(data, m => m.age < 30));
+
+
+/*
+const _compact = data => {
+    // filter(data, identity);
+    return filter(identity)(data);
+};
+ */
+const _compact = filter(identity);
+console.log(_compact([0, false, null, undefined, {}, []]));
+
+
+const _find = (data, cond) => {
+    const list = keys(data);
+    for (let i = 0; i < list.length; i++) {
+        const m = data[list[i]];
+        if (cond(m)) return m;
+    }
+};
+console.log(_find(data, m => m.name === '강백호'));
+console.log(_find(data, m => m.name === '송송송'));
+
+
+const _find_index = (data, cond) => {
+    const list = keys(data);
+    for (let i = 0; i < list.length; i++) {
+        const m = data[list[i]];
+        if (cond(m)) return list[i];
+    }
+    return -1;
+};
+console.log(_find_index(data, m => m.name === '송태섭'));
+console.log(_find_index(data, m => m.name === '조로'));
+console.log(_find_index(data, m => m.name === '권준일'));
+
+
+const _some = (data, cond) => {
+    const list = keys(data);
+    for (let i = 0; i < list.length; i++) {
+        const m = data[list[i]];
+        if (cond(m)) return true;
+    }
+    return false;
+};
+console.log(_some(data, m => m.age > 100));
+console.log(_some(data, m => m.age < 0));
+
+
+const _every = (data, cond) => {
+    return _find_index(data, negate(cond)) === -1;
+};
+console.log(_every(data, m => m.age > 100));
+console.log(_every(data, m => m.age > 0));
+
+
+
--- src/lecture/functional/lesson_1/chapter_1/fp.js
+++ src/lecture/functional/lesson_1/chapter_1/fp.js
@@ -1,5 +1,7 @@
 /**
  * 함수형 프로그래밍은 가능한 에러를 일으키지 않고 납득할 만한 결과로 반환한다.
+ * null 이 아닌 object 에 대해서만 진행한다.
+ * function 은 포함되지 않는다.
  *
  * @param obj
  * @returns {string[]|*[]}
@@ -10,6 +12,8 @@ const _keys = obj => (typeof obj === 'object' && obj !== null)
     : [];
 
 /**
+ * 배열뿐만 아니라 객체에 대해서도 적용한다.
+ * 함수에 적용되는 것은 값이다.
  *
  * @param data
  * @param func
@@ -53,9 +57,11 @@ const _curryr = (func) => {
 };
 
 /**
+ * 예외 상황에 대하여 에러를 발생시키지 않고 빈 배열을 반환한다.
  *
- * @type {function(*, *): (*|(function(*): *))}
- * @private
+ * @param data
+ * @param cond
+ * @returns {*[]}
  */
 const _filter = _curryr((data, cond) => {
     const ret = [];
@@ -64,9 +70,11 @@ const _filter = _curryr((data, cond) => {
 });
 
 /**
+ * 예외 상황에 대하여 에러를 발생시키지 않고 빈 배열을 반환한다.
  *
- * @type {function(*, *): (*|(function(*): *))}
- * @private
+ * @param data
+ * @param convert
+ * @returns {*[]}
  */
 const _map = _curryr((data, convert) => {
     const ret = [];
@@ -100,32 +108,40 @@ const _reduce = (data, func, start) => {
 };
 
 /**
+ * 함수들의 배열을 인자로 받는 특수한 reduce 이다.
  *
  * @param fns
  * @returns {function(*): *}
  * @private
  */
-const _pipe = (...fns) => arg => reduce(fns, (arg, fn) => fn(arg), arg);
+const _pipe = (...fns) => arg => {
+    return _reduce(fns, (arg, fn) => fn(arg), arg);
+};
 
 /**
+ * 첫번째 함수 인자가 주어진 특수한 pipe 이다.
  *
  * @param arg
  * @param fns
  * @returns {*}
  * @private
  */
-const _go = (arg, ...fns) => _pipe(...fns)(arg);
+const _go = (arg, ...fns) => {
+    return _pipe(...fns)(arg);
+};
 
 /**
  *
- * @type {function(*, *): (*|(function(*): *))}
- * @private
+ * @param obj
+ * @param key
+ * @returns {undefined|*}
  */
 const _get = _curryr((obj, key) => {
     return obj == null ? undefined : obj[key];
 });
 
 /**
+ * 항등함수
  *
  * @param m
  * @private
@@ -133,20 +149,106 @@ const _get = _curryr((obj, key) => {
 const _identity = m => m;
 
 /**
+ * 부정함수
  *
- * @param m
- * @returns {boolean}
+ * @param cond
+ * @returns {function(*): boolean}
  * @private
  */
-const _negative = m => !m;
+const _negate = cond => (arg => !cond(arg));
+
+/**
+ * 객체에서 값만을 뽑아낸다.
+ *
+ * @type {*|(function(*): *)}
+ * @private
+ */
+const _values = _map(_identity);
+
+/**
+ * 배열에서 각 요소의 특정 속성만을 뽑아내어 배열을 만든다.
+ *
+ * @type {function(*, *): (*|(function(*): *))}
+ * @private
+ */
+const _pluck = _curryr((data, key) => {
+    return _map(data, get(key));
+});
 
 /**
  *
  * @param data
- * @returns {*|(function(*): *)}
+ * @param cond
  * @private
  */
-const _values = data => _map(_identity);
+const _reject = _curryr((data, cond) => {
+    return _filter(data, _negate(cond));
+});
+
+/**
+ *
+ * @private
+ */
+const _compact = _filter(_identity);
+
+/**
+ *
+ * @param data
+ * @param cond
+ * @returns {*}
+ * @private
+ */
+const _find = _curryr((data, cond) => {
+    const list = _keys(data);
+    for (let i = 0; i < list.length; i++) {
+        const m = data[list[i]];
+        if (cond(m)) return m;
+    }
+    return null;
+});
+
+/**
+ *
+ * @param data
+ * @param cond
+ * @returns {number|*}
+ * @private
+ */
+const _find_index = _curryr((data, cond) => {
+    const list = _keys(data);
+    for (let i = 0; i < list.length; i++) {
+        const m = data[list[i]];
+        if (cond(m)) return list[i];
+    }
+    return -1;
+});
+
+/**
+ *
+ * @param data
+ * @param cond
+ * @returns {boolean}
+ * @private
+ */
+const _some = _curryr((data, cond) => {
+    const list = keys(data);
+    for (let i = 0; i < list.length; i++) {
+        const m = data[list[i]];
+        if (cond(m)) return true;
+    }
+    return false;
+});
+
+/**
+ *
+ * @param data
+ * @param cond
+ * @returns {boolean}
+ * @private
+ */
+const _every = _curryr((data, cond) => {
+    return _find_index(data, _negate(cond)) === -1;
+});
 
 module.exports = {
     keys: _keys,
@@ -161,6 +263,13 @@ module.exports = {
     go: _go,
     get: _get,
     identity: _identity,
-    negative: _negative,
+    negate: _negate,
     values: _values,
+    pluck: _pluck,
+    reject: _reject,
+    compact: _compact,
+    find: _find,
+    find_index: _find_index,
+    some: _some,
+    every: _every,
 }
