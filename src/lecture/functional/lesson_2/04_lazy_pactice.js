4815b815df569aded5babd4fa64185c26adbcd50
june1 <likejunil@gmail.com>
2023-01-15 21:05:27 +0900

    함수형 프로그래밍 진짜 재밌다.

---
+++ src/lecture/functional/lesson_2/04_lazy_pactice.js
@@ -0,0 +1,16 @@
+/**
+ * query_str
+ * join
+ * entries
+ * find
+ * L.map 으로 _.map 작성
+ * L.filter 으로 _.filter 작성
+ * take_all
+ *
+ * L.flatten
+ * is_iterable
+ * L.deep_flatten
+ *
+ * L.flat_map
+ * 
+ */
