09c5c67f4b864ce990f1b42d6b01a3439c498a9b
권준일 <likejunil@gmail.com>
2023-02-24 07:43:50 +0900

    FEAT: NodeBird 해시태그 관련 작업을 마무리하라

--- src/lecture/zerocho/proj/nodebird/controller/pageCont.js
+++ src/lecture/zerocho/proj/nodebird/controller/pageCont.js
@@ -1,5 +1,6 @@
 const {User} = require('../repository/sequelize/model/user.js');
 const {Post} = require('../repository/sequelize/model/post.js');
+const {Hashtag} = require('../repository/sequelize/model/hashtag.js');
 
 const main = async (req, res, next) => {
     try {
@@ -30,8 +31,37 @@ const profile = (req, res, next) => {
     res.render('profile', {title: '내 정보'});
 };
 
+const hashtag = async (req, res, next) => {
+    /* 요청 쿼리가 올라왔는가 */
+    const {hashtag: tag} = req.query;
+    if (!tag) {
+        return res.redirect('/');
+    }
+    
+    /* 검색 데이터가 존재하는가 */
+    const load = await Hashtag.findOne({where: {tag}});
+    if (!load) {
+        return res.redirect('/');
+    }
+    
+    /* 올바른 요청과 응답 결과 */
+    const posts = await load.getPosts({
+        include: [{
+            model: User,
+            attributes: ['id', 'nick'],
+        }],
+        order: [['createdAt', 'DESC']],
+    });
+    
+    res.render('main', {
+        title: `${tag} | NodeBird`,
+        twits: posts,
+    });
+};
+
 module.exports = {
     main,
     join,
     profile,
+    hashtag,
 };
--- src/lecture/zerocho/proj/nodebird/router/pageRout.js
+++ src/lecture/zerocho/proj/nodebird/router/pageRout.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const {main, join, profile} = require('../controller/pageCont.js');
+const {main, join, profile, hashtag} = require('../controller/pageCont.js');
 const {isLoggedIn, isNotLoggedIn} = require('../middleware/passport/checkConnect.js');
 const {User} = require('../repository/sequelize/model/user.js');
 
@@ -7,8 +7,6 @@ const router = Router();
 
 /* 해당 라우터의 요청에 대하여 공통적으로 처리해야 할 사항 */
 router.use(async (req, res, next) => {
-    res.locals.user = req.user;
-    
     /* passport 의 deserialize 에서 한꺼번에 구하는 방법도 있다. */
     /* 하지만 req.user 의 크기가 커진다는 단점을 고려해야 한다. */
     let user = req.user;
@@ -34,6 +32,7 @@ router.use(async (req, res, next) => {
         }
     }
     
+    res.locals.user = user;
     res.locals.followerCount = user?.Followers?.length || 0;
     res.locals.followingCount = user?.Followings?.length || 0;
     res.locals.followingIdList = user?.Followings?.map(m => m.id) || [];
@@ -48,5 +47,6 @@ router.use(async (req, res, next) => {
 router.get("/", main);
 router.get("/join", isNotLoggedIn, join);
 router.get("/profile", isLoggedIn, profile);
+router.get("/hashtag", isLoggedIn, hashtag);
 
 module.exports = router;
