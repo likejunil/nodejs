e4350ac9df6841cea04f1736804a12e9f9f364ab
june1 <likejunil@gmail.com>
2023-03-13 20:22:43 +0900

    밴드 진행 중..

--- src/project/board/controller/band.js
+++ src/project/board/controller/band.js
@@ -1,5 +1,6 @@
 const {Band} = require('../repository/sequelize/model/band.js');
 const {succeed} = require('./common.js');
+const {User} = require("../repository/sequelize/model/user");
 
 /**
  1. 밴드 생성
@@ -8,23 +9,32 @@ const {succeed} = require('./common.js');
  4. 밴드 삭제
  */
 
-const attributes = [
-    'id',
-    'name',
-    'desc',
-    'ownerId',
-];
+const attributes = ['id', 'name', 'desc', 'ownerId',];
 
 const getBand = async (query) => {
     const {id, name} = query;
     const where = {};
-    if (id != null) where.id = id;
-    else if (name != null) where.name = name;
+    if (id != null) where.id = id; else if (name != null) where.name = name;
     
     return await Band.findOne({where, attributes});
 };
 
-const getBands = () => {
+const getBands = async (query) => {
+    const {name, desc, ownerId} = query;
+    const {page, size, offset, limit, sort} = query;
+    
+    const where = {};
+    if (name != null) where.name = name;
+    if (desc != null) where.desc = desc;
+    if (ownerId != null) where.ownerId = ownerId;
+    
+    const {count, rows: bands} = await Band.findAndCountAll({
+        where, attributes, offset, limit, sort, include: {
+            model: User, attributes: ['id', 'nick', 'name'],
+        },
+    });
+    
+    return {count, page, size, bands}
 };
 
 /**
@@ -41,7 +51,9 @@ const createOne = async (req, res, next) => {
  * GET /band
  */
 const findBands = async (req, res, next) => {
-    const {size, page, sort} = req.query;
+    const {query} = req;
+    const {count, page, size, bands} = await getBands(query);
+    res.json(succeed(bands, page, size, count));
 };
 
 /**
@@ -53,7 +65,5 @@ const findById = async (req, res, next) => {
 };
 
 module.exports = {
-    createOne,
-    findBands,
-    findById,
+    createOne, findBands, findById,
 };
